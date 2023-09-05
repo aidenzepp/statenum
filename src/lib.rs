aee185cf16a13441f327b5b3e0581c0cc99ce6e1
Aiden Zepp <aiden.zepp@protonmail.com>
2023-09-05 10:41:47 -0400

    Initial concept works.

---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_attribute]
+pub fn statenum(_args: TokenStream, item: TokenStream) -> TokenStream {
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(item as DeriveInput);
+
+    // Ensure we're working with an enum
+    let data = match input.data {
+        syn::Data::Enum(e) => e,
+        _ => panic!("statenum can only be used with enums"),
+    };
+
+    // Generate a struct and State implementation for each variant
+    let structs = data.variants.iter().map(|variant| {
+        let struct_name = syn::Ident::new(&format!("{}", variant.ident), variant.ident.span());
+        quote! {
+            pub struct #struct_name;
+            impl State for #struct_name {}
+        }
+    });
+
+    // Define the State trait
+    let state_trait = quote! {
+        pub trait State {}
+    };
+
+    // Combine everything into a single TokenStream
+    let expanded = quote! {
+        #state_trait
+        #(#structs)*
+    };
+
+    // Done
+    TokenStream::from(expanded)
+}
